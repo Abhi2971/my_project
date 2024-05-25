0114f21378b5b61f5c92e28dd691faadf57a4044
Abhishek <abhishelke297127@gmail.com>
2024-05-25 20:58:34 +0530

    25/05/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  h2Title="hello";
+  html="my name is abhi";
   title = 'app_01';
+  buttonClicked(ev:any){
+    alert("you clicked me");
+  }
+  ifmouseOver=false;
+  mouseOver(ev:any){
+    console.log(ev);
+    this.ifmouseOver=true;
+  }
+  mouseOut(ev:any){
+    this.ifmouseOver=false;
+  }
+   stdArr = [{
+    name : "Abhishek",
+    gender :"M",
+    dob :"31/08/1999",
+    address :"Satara"
+  },{
+    name :"ganesh",
+    gender :"Male",
+    dob :"25.12.1999",
+    address :"bangladesh"
+  }];
 }
